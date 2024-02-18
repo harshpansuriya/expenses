4edaf8f8fbf6a91f7e4213676bb5ed8d3c0ef80c
Harsh Pansuriya <harshpansuriya123@gmail.com>
2024-02-17 20:17:25 -0600

    feat: add Firebase authentication to UserAuthForm component

---
+++ frontend/frontend/firebaseconfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCjWm6w1dxNPFn5Mi0fHrRYy1ox70CPF2w",
+    authDomain: "ex-me-414423.firebaseapp.com",
+    projectId: "ex-me-414423",
+    storageBucket: "ex-me-414423.appspot.com",
+    messagingSenderId: "347853190610",
+    appId: "1:347853190610:web:6528700aad263b4e71c40b",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
--- frontend/frontend/src/components/AuthForm/UserAuthForm.tsx
+++ frontend/frontend/src/components/AuthForm/UserAuthForm.tsx
@@ -8,19 +8,56 @@ import { Button } from "../ui/button";
 import { Input } from "../ui/input";
 import { Label } from "../ui/label";
 
+import { useState } from "react";
+
+// Authentication
+import { initializeApp } from "firebase/app";
+import { onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
 interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
 
-export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
+export function UserAuthForm(
+    this: any,
+    { className, ...props }: UserAuthFormProps
+) {
     const [isLoading, setIsLoading] = React.useState<boolean>(false);
 
-    async function onSubmit(event: React.SyntheticEvent) {
-        event.preventDefault();
-        setIsLoading(true);
+    // async function onSubmit(event: React.SyntheticEvent) {
+    //     event.preventDefault();
+    //     setIsLoading(true);
 
-        setTimeout(() => {
-            setIsLoading(false);
-        }, 3000);
-    }
+    //     setTimeout(() => {
+    //         setIsLoading(false);
+    //     }, 3000);
+    // }
+
+    const firebaseConfig = {
+        apiKey: "AIzaSyCjWm6w1dxNPFn5Mi0fHrRYy1ox70CPF2w",
+        authDomain: "ex-me-414423.firebaseapp.com",
+    };
+    const app = initializeApp(firebaseConfig);
+    const auth = getAuth(app);
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+
+    const onSubmit = (event: { preventDefault: () => void }) => {
+        //check if passwords match. If they do, create user in Firebase
+        // and redirect to your logged in page.
+
+        createUserWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                console.log("Success. The user is created in Firebase");
+            })
+            .catch((error) => {
+                // An error occurred. Set error message to be displayed to user
+                setError(error.message);
+            });
+
+        event.preventDefault();
+    };
 
     return (
         <div className={cn("grid gap-6", className)} {...props}>
@@ -38,6 +75,25 @@ export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
                             autoComplete="email"
                             autoCorrect="off"
                             disabled={isLoading}
+                            value={email}
+                            onChange={(event) => setEmail(event.target.value)}
+                        />
+                    </div>
+                    <div className="grid gap-1">
+                        <Label className="sr-only" htmlFor="password">
+                            Password
+                        </Label>
+                        <Input
+                            id="password"
+                            placeholder="********"
+                            type="password"
+                            autoCapitalize="none"
+                            autoCorrect="off"
+                            disabled={isLoading}
+                            value={password}
+                            onChange={(event) =>
+                                setPassword(event.target.value)
+                            }
                         />
                     </div>
                     <Button disabled={isLoading}>
